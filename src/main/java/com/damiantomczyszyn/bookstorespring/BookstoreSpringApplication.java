e1553b3ac39dcfbf5b7ce73af5d8b64b99274d84
Damian Tomczyszyn <damitom@o2.pl>
2023-06-11 09:12:03 +0200

    init

---
+++ src/main/java/com/damiantomczyszyn/bookstorespring/BookstoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.damiantomczyszyn.bookstorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/damiantomczyszyn/bookstorespring/BookstoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damiantomczyszyn.bookstorespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
