0f7a7481868141c9eece8a1efa812ae6ee7400c8
Damian Tomczyszyn <damitom@o2.pl>
2023-06-11 09:24:04 +0200

    models

---
+++ src/main/java/com/damiantomczyszyn/bookstorespring/model/Item.java
@@ -0,0 +1,32 @@
+package com.damiantomczyszyn.bookstorespring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private String imgUrl;
+
+    public Item(String name, BigDecimal price, String imgUrl) {
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+}
